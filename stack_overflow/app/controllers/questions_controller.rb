8b9042006bcd3a07ee91c1b60eb0e623d44d6d7a
Leandra Kim & Leandra Kim <pair+lea3182.lea3182@devbootcamp.com>
2015-05-03 17:20:29 -0700

    additional commit

--- stack_overflow/app/controllers/questions_controller.rb
+++ stack_overflow/app/controllers/questions_controller.rb
@@ -1,2 +1,27 @@
 class QuestionsController < ApplicationController
+
+  def index
+    @questions = Question.all
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+    @question = Question.find(params[:id])
+    @answer = Answer.where(question_id: params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
 end
