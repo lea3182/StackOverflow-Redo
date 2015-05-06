9ee9d581689eec4bb1842f188927e4000e739559
Leandra Kim & Leandra Kim <pair+lea3182.lea3182@devbootcamp.com>
2015-05-05 18:34:52 -0700

    Add edit and delete button for answers. Add back button

--- stack_overflow/app/controllers/answers_controller.rb
+++ stack_overflow/app/controllers/answers_controller.rb
@@ -13,4 +13,28 @@ class AnswersController < ApplicationController
     #   redirect_to answers_path
     # end
   end
+  def show
+  end
+
+  def edit
+    @answer = Answer.find(params[:id])
+    @question = Question.find(params[:question_id])
+  end
+
+  def update
+    @question = Question.find(params[:question_id])
+    @answer = Answer.find(params[:id])
+    @answer.update(title: params[:answer][:title],
+                   content: params[:answer][:content])
+
+    redirect_to @question
+  end
+
+  def destroy
+    @question = Question.find(params[:question_id])
+    @answer = Answer.find(params[:id])
+    @answer.destroy
+
+    redirect_to @question
+  end
 end
